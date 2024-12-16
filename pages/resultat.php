32062d1412d6e9df4de15b328740d001514a65bc
naimdeb <naimdebard02@gmail.com>
2024-12-16 16:15:04 +0100

    test fix

--- pages/resultat.php
+++ pages/resultat.php
@@ -8,38 +8,102 @@ if (!isset($_SESSION["nbOfQuestions"]) && !isset($_SESSION["nbOfCorrectQuestions
 }
 
 
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/output.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Quizz404</title>
+  <link rel="icon" href="./image/_.ico">
+  <link rel="stylesheet" href="../css/output.css">
 </head>
+
 <body>
+<div id="app">
+  <div id="star-container">
+
+  <header class="flex w-100 justify-around pt-5 items-center opacity-[1] z-2">
+
     
+    <a class="text-white font-first-font font-extrabold" href="./pages/formulaire.php">
+    <div class="w-[9%] h-[70px] items-center flex justify-center btn">
+    Inscription
+    </div>
+    </a>
+
+    <div class="w-[50%] p-5 items-center text-center ">
+      <h2 class="text-8xl font-extrabold font-first-font">
+        <span>
+          < 
+        </span>
+        <span>Quiz</span>
+        <span class="text-[#8344bc]">405</span>
+        <span>/></span>
+      </h2>
+    </div>
+ 
+      
+    <a class="text-white font-first-font font-extrabold" href="./pages/connexion.php">
+    <div class="w-[9%] h-[70px] items-center flex justify-center btn">
+    Connexion
+    </div>
+    </a>
+  </header>
+  
+<main>
+
+    <div id="star-pattern"></div>
+    <div id="star-gradient-overlay">
+      
+    </div>
+  </div>
+  <div id="stripe-container">
+    <div id="stripe-pattern">
+<div class="h-[100%] pt-[250px]">
 
 
-<div class="bg-slate-100 w-[500px] h-[500px] m-auto mt-32 shadow-md p-8">
+
+  <div class="flex flex-col gap-9 text-center w-100 items-center">   
+
+  <div class="bg-slate-100 w-[500px] h-[500px] m-auto shadow-md p-8">
 
     <h3 class="text-center">Résultats</h3>
 
         <p> <?=$_SESSION["nbOfCorrectQuestions"]?> /  <?= $_SESSION["nbOfQuestions"] ?> questions correctes</p>
 
-        <p class="text-end pt-16"> Votre score : </p>
+        <p class="text-end pt-16"> Votre score :  y'a pas</p>
 
 
-        <a href="./choixQuizz.php" class="p-2 bg-sky-400">Retourner au choix des quizz</a>
+    <a class="text-white font-first-font font-extrabold" href="./choixQuizz.php">
+    <div class="h-[70px] items-center flex justify-center bg-slate-600 text-nowrap">
+        Retourner au choix des quizz
+    </div>
+    </a>
 
 
 
+    </div>
+
+
+
+ 
+  </div>
 </div>
-
-
+    </div>
+  </div>
+</main>
 
 
 </body>
+
 </html>
+
+
+?>
+
+
+
+
